e8accc8447b5c28e7114634270edc3ce85b9eb28
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2019-04-11 23:37:17 -0300

    Add categories seed

--- lib/data/sqflite_database.dart
+++ lib/data/sqflite_database.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -28,5 +30,16 @@ class SqfliteDatabase {
             "FOREIGN KEY (category_id) REFERENCES Category(id)"
             ") "
     );
+
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['alimentação', Color(0xfff5a623).value]);
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['educação', Color(0xff7ed321).value]);
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['financeiro', Color(0xffe24a8b).value]);
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['habitação', Color(0xffb83030).value]);
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['lazer', Color(0xff4adbe2).value]);
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['saúde', Color(0xff4a90e2).value]);
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['trabalho', Color(0xff3eba65).value]);
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['transporte', Color(0xffbb37d6).value]);
+    await db.rawInsert("INSERT INTO Category (title, color) VALUES(?,?)", ['vestuário', Color(0xffedc100).value]);
+
   }
 }
